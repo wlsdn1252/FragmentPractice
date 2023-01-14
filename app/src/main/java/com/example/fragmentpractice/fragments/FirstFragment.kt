047a01454bcfe75cd9e6a9a2d7a8e15429c0c1f0
강진우 <wlsdn1252@naver.com>
2023-01-14 18:55:42 +0900

    프래그먼트 코틀린 파일, xml파일 결합 작업 onCreate에서 발생

---
+++ app/src/main/java/com/example/fragmentpractice/fragments/FirstFragment.kt
@@ -0,0 +1,20 @@
+package com.example.fragmentpractice.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fragmentpractice.R
+
+class FirstFragment : Fragment(){   // 프래그먼트 상속
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
