3539d7152c77fb8d20ae10f439ded209aa244e87
강진우 <wlsdn1252@naver.com>
2023-01-14 19:33:52 +0900

    두번째 프래그먼트 토스트메세지 기능 구현

--- app/src/main/java/com/example/fragmentpractice/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/fragmentpractice/fragments/SecondFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice.R
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment() {
 
@@ -17,9 +20,14 @@ class SecondFragment : Fragment() {
      return inflater.inflate(R.layout.fragment_second,container,false)
     }
 
+    // 프래그 먼트 동작에 관한 함수 오버라이딩
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        secondFragmentBtn.setOnClickListener {
+            Toast.makeText(context, "두번째 프래그먼트 토스트메세지", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 
